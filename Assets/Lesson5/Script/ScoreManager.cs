7c04229445d137d02213e656b7a57e2c3b759561
TsukasaOnose <70473575+TsukasaOnose@users.noreply.github.com>
2020-09-12 17:43:07 +0900

    Lesson5課題試行錯誤中

---
+++ Assets/Lesson5/Script/ScoreManager.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ScoreManager : MonoBehaviour
+{
+    private Text scoreText;
+    private int oldScore;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        scoreText = GetComponent<Text>();
+        if (scoreText != null && GManager.instance != null)
+        {
+            scoreText.text = GManager.instance.score.ToString();
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (scoreText != null && GManager.instance != null)
+        {
+            if (oldScore != GManager.instance.score)
+            {
+                scoreText.text = GManager.instance.score.ToString();
+                oldScore = GManager.instance.score;
+            }
+        }
+    }
+}
---
+++ Assets/Lesson5/Script/ScorePoints.cs
@@ -0,0 +1,64 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.UI;
+using UnityEngine;
+
+public class ScorePoints : MonoBehaviour
+{
+    public int myScore;
+    //SmallStarの点数
+    public int smallstarPoints = 10;
+    //LargeStarの点数
+    public int largestarPoints = 50;
+    //SmallCloudの点数
+    public int smallcloudPoints = 20;
+    //LargeCloudの点数
+    public int largecloudpoints = 80;
+
+    private void Start()
+    {
+        //SmallStarに当たった時
+        if (tag == "SmallStarTag")
+        {
+            if (GManager.instance != null)
+            {
+                GManager.instance.score += smallstarPoints;
+            }
+        }
+        //LargeStarに当たった時
+        if (tag == "LargeStarTag")
+        {
+            if (GManager.instance != null)
+            {
+                GManager.instance.score += largestarPoints;
+            }
+        }
+        //SmallCloudに当たった時
+        if (tag == "SmallCloud")
+        {
+            if (GManager.instance != null)
+            {
+                GManager.instance.score += smallcloudPoints;
+            }
+        }
+        //LargeColudに当たった時
+        if (tag == "LargeCloud")
+        {
+            if (GManager.instance != null)
+            {
+                GManager.instance.score += largecloudpoints;
+            }
+        }
+    }
+
+    private void Update()
+    {
+       
+    }
+
+    //衝突時に呼ばれる関数
+    void OnCollisionEnter(Collision collision)
+    {
+        
+    }
+}
